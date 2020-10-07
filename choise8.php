ca1abbafb314e26c86f857c567c6da181b2118dc
Hiro-0403 <63594128+Hiro-0403@users.noreply.github.com>
2020-10-07 21:05:39 +0900

    異なるデータ型の比較

---
+++ choise8.php
@@ -0,0 +1,11 @@
+<?php
+$diseA = 6;
+$diseB = "6";
+
+$diseC = (int)$diseB;
+
+if ($diseA ===$diseC) {
+  echo "Win";
+} else {
+  echo"Lose";
+}
---
+++ choise9.php
@@ -0,0 +1,9 @@
+<?php
+$diseA = 6;
+$diseB = "6";
+
+if ($diseA === $diseB) {
+  echo "Win";
+} else {
+  echo "Lose";
+}
