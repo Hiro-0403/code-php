915008f65ae47f7ce0c7181ce52e9d3031f32419
Hiro-0403 <63594128+Hiro-0403@users.noreply.github.com>
2020-10-10 09:17:23 +0900

    配列要素トレーニング

---
+++ string_tr3.php
@@ -0,0 +1,7 @@
+<?php
+$str = "Hyper-Text-Markup-Language";
+// ["Hyper", "Text", "Markup", "Language"]
+$array = explode("-", $str);
+for ($i = 0; $i < count($array); $i ++) {
+  echo substr($array[$i], 0, 1);
+}
