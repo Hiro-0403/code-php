abdf10ea8d96864bb0caf39064e27c6a9573d8a6
Hiro-0403 <63594128+Hiro-0403@users.noreply.github.com>
2020-10-13 06:37:46 +0900

    HPPTレスポンス
    リダイレクト

--- login.php
+++ login.php
@@ -3,19 +3,9 @@ $id = $_POST["id"];
 $password = $_POST["password"];
 
 
-$message = "NG";
-if ($id === "Andy" && $password === "secret") {
-  $message = "OK";
-}
-?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <title>sample php</title>
-</head>
-<body>
-  <h1>Login<?php echo $message; ?></h1>
-</body>
-</html>
+if ($id === "Andy" && $password === "secret") {
+  header("Location: menu.php");
+} else {
+  header("Location: login.html");
+}
---
+++ menu.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>PHP Sample</title>
+</head>
+<body>
+  <h3>Menu</h3>
+  <hr>
+  <ul>
+    <li>ToDo</li>
+  </ul>
+</body>
+</html>
