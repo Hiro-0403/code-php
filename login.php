c14a13ecb71f5f46994ceef1f6b0c6444dba3238
Hiro-0403 <63594128+Hiro-0403@users.noreply.github.com>
2020-10-14 06:35:14 +0900

    session1

--- login.php
+++ login.php
@@ -3,8 +3,11 @@ $id = $_POST["id"];
 $password = $_POST["password"];
 
 
-
 if ($id === "Andy" && $password === "secret") {
+  session_start();
+  $_SESSION["id"] = $id;
+  $_SESSION["time"] = date("Y-m-d H:i:s");
+
   header("Location: menu.php");
 } else {
   header("Location: login.html");
--- menu.php
+++ menu.php
@@ -1,3 +1,9 @@
+<?php
+session_start();
+$id = $_SESSION["id"];
+$time = $_SESSION["time"];
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,7 +11,8 @@
   <title>PHP Sample</title>
 </head>
 <body>
-  <h3>Menu</h3>
+  <h3>Menu - Hello<?php echo $id; ?></h3>
+  <h5>Login at <?php echo $time; ?></h5>
   <hr>
   <ul>
     <li>ToDo</li>
